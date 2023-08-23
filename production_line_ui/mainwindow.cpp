6c5ba84b6b5dd84085ed45e0f0f065a0ae659f76
Elisa Harjunpää <elisa@localhost.localdomain>
2023-08-23 09:41:49 +0300

    QDateTimeAxis(time is placed on the x-axis), button click-event updates the chart

--- production_line_ui/mainwindow.cpp
+++ production_line_ui/mainwindow.cpp
@@ -8,22 +8,43 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    QLineSeries *series =  new QLineSeries();  // Constructor for QLineSeries
-    series->append(0, 25);  // Append some hard coded examplle data to series
-    series->append(2, 38);
-    series->append(4, 55);
-    series->append(6, 63);
-    series->append(8, 76);
+        axis_x = new QDateTimeAxis();
+        axis_y = new QValueAxis();
+        chart = new QChart();
+        series = new QLineSeries();
 
-    auto chart = new QChart();
-    chart->addSeries(series);
-    chart->createDefaultAxes();
-    chart->setTitle("Temperature");
-    chart->legend()->setAlignment(Qt::AlignBottom);
+        // Y-axis
+        axis_y->setLabelFormat("%i");
+        axis_y->setTitleText("Celcius degrees");
+        axis_y->setRange(0, 90);
 
-    auto chart_view = new QChartView(chart);
-    chart_view->setRenderHint(QPainter::Antialiasing);
-    chart_view->setParent(ui->temperature_chart);
+        // X-axis
+        axis_x->setTickCount(10);
+        axis_x->setFormat("hh:mm:ss");
+        axis_x->setTitleText("Time");
+        axis_x->setMax(QDateTime::fromString("230000",  "hhmmss"));
+        axis_x->setMin(QDateTime::fromString("200000", "hhmmss"));
+
+        // Series
+        series->append(QDateTime::fromString("200000", "hhmmss").toMSecsSinceEpoch(), 20);
+        series->append(QDateTime::fromString("201500", "hhmmss").toMSecsSinceEpoch(), 20);
+        series->append(QDateTime::fromString("203000", "hhmmss").toMSecsSinceEpoch(), 20);
+
+        // Chart
+        chart->legend()->hide();
+        chart->setTitle("Temperature");
+        chart->addAxis(axis_x, Qt::AlignBottom);
+        chart->addAxis(axis_y, Qt::AlignLeft);
+        chart->addSeries(series);
+
+        series->attachAxis(axis_x);
+        series->attachAxis(axis_y);
+
+        // ChartView
+        chart_view = new QChartView(chart);
+        chart_view->setRenderHint(QPainter::Antialiasing);
+        chart_view->setParent(ui->temperature_chart);
+
 
     MQTTClient test("broker.hivemq.com", "test");
     test.connect();
@@ -40,5 +61,15 @@ MainWindow::~MainWindow()
 void MainWindow::on_pushButton_clicked()
 {
     test->publish("TestTopic123", "Hello world");
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    series->clear();
+    series->append(QDateTime::fromString("200000", "hhmmss").toMSecsSinceEpoch(), 28);
+    series->append(QDateTime::fromString("210000", "hhmmss").toMSecsSinceEpoch(), 39);
+    series->append(QDateTime::fromString("220000", "hhmmss").toMSecsSinceEpoch(), 55);
+    series->append(QDateTime::fromString("223000", "hhmmss").toMSecsSinceEpoch(), 70);
 }
 
