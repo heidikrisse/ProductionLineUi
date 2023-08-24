43101837b3f2de2cfb432663623fb6c96a262c66
heidikrisse <heidi.kristiina90@gmail.com>
2023-08-24 23:13:56 +0300

    Setting up doctest with QtCreator

--- production_line_ui/unit_test/unit_tests.cpp
+++ production_line_ui/unit_test/unit_tests.cpp
@@ -1,2 +1,15 @@
-#define DOCTEST_CONFIG_IMPLEMENT // _WITH_MAIN
+#define DOCTEST_CONFIG_IMPLEMENT
 #include "../include/doctest.h"
+#include "../include/json_parser.h"
+#include "../include/mqtt_client.h"
+
+TEST_CASE("Example Test Case")
+{
+    int a = 2;
+    int b = 3;
+
+    REQUIRE(a + b == 5);
+}
+
+
+
